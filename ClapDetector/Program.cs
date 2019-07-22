ecde60ecd25c6c6f108973e48f2401be649d171a
Štěpán Beneš <benes.stepan@gmail.com>
2019-07-22 23:18:39 +0200

    Add project files.

---
+++ ClapDetector/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenAL;
+using System;
+using System.Linq;
+
+namespace ClapDetector
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var device = Alc.OpenDevice(null);
+			var context = Alc.CreateContext(device, null);
+
+			Alc.MakeContextCurrent(context);
+		}
+	}
+}
