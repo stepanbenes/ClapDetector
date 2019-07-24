881418ecacea8cefc006359e3c78957237f59a76
Štěpán Beneš <benes.stepan@gmail.com>
2019-07-24 15:41:55 +0200

    Write capture keyword to wav file.

--- ClapDetector/Program.cs
+++ ClapDetector/Program.cs
@@ -1,14 +1,15 @@
 ﻿using OpenAL;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace ClapDetector
 {
 	class Program
 	{
-		static async Task Main(string[] args)
+		static void Main(string[] args)
 		{
-
 			// inspired by: https://stackoverflow.com/questions/4087727/openal-how-to-create-simple-microphone-echo-programm
 
 			var audioDevice = Alc.OpenDevice(null);
@@ -19,23 +20,32 @@ namespace ClapDetector
 			var context = Alc.CreateContext(audioDevice, null);
 			Alc.MakeContextCurrent(context);
 
-			const int frequency = 22050;
+			const int frequency = 44100;
 
 			var captureDevice = Alc.CaptureOpenDevice(devicename: null, frequency, format: Al.FormatMono16, buffersize: frequency);
 
+			string deviceName = Alc.GetString(captureDevice, Alc.DeviceSpecifier);
+
+			Console.WriteLine($"{deviceName} is open for capture.");
+
 			Alc.CaptureStart(captureDevice);
 
-			Console.WriteLine("Clap detector is listening..."); // need new line for intensity visualization
+			Console.WriteLine("listening for keywords..."); // need new line for intensity visualization
 
 			short[] buffer = new short[frequency];
+			const int keywordLength = frequency * 1;
+			double? previousRMS = null;
+
+			List<short> keywordSamples = new List<short>(capacity: keywordLength);
+			bool isRecording = false;
 			unsafe
 			{
 				fixed (int* samplesAvailable = new int[1])
 				fixed (short* bufferPointer = buffer)
 				{
 					IntPtr samplesAvailablePointer = new IntPtr((void*)samplesAvailable);
-					int captureCount = 2048;
-					while (true)
+					int captureCount = 2048; // (captureCount / frequency) seconds
+					while (keywordSamples.Count < keywordLength)
 					{
 						Alc.GetIntegerv(captureDevice, Alc.EnumCaptureSamples, 1, samplesAvailablePointer);
 						int samplesAvailableCount = samplesAvailable[0];
@@ -43,62 +53,88 @@ namespace ClapDetector
 						{
 							Alc.CaptureSamples(captureDevice, (void*)bufferPointer, samplesAvailableCount);
 
-							// ... do something with the buffer
+							// TODO: ... do something with the buffer
 							double rms = calculateRMS(buffer, samplesAvailableCount);
+
 							double maxAmplitude = short.MaxValue;
 							double intensity = rms / maxAmplitude;
 							printIntensity(intensity);
+
+							// compare current rms with previous values to find beginning of a keyword
+							const double factor = 10;
+							if (rms > previousRMS * factor) // beginning of window
+							{
+								isRecording = true;
+								Console.Beep(10000, 10);
+							}
+
+							if (isRecording)
+							{
+								for (int i = 0; i < samplesAvailableCount; i++)
+								{
+									keywordSamples.Add(buffer[i]);
+								}
+							}
+
+							previousRMS = rms;
 						}
 					}
 				}
 			}
+			Alc.CaptureStop(captureDevice);
 			resetConsoleSettings();
 
-			Alc.CaptureStop(captureDevice);
+			// write output
+			{
+				var wavFile = new WavFile("keyword.wav") { SamplesPerSecond = frequency, SamplesTotalCount = keywordSamples.Count };
+				wavFile.WriteMono16bit(keywordSamples.ToArray());
+			}
 
+			// clean up
 			Alc.CaptureCloseDevice(captureDevice);
 			Alc.MakeContextCurrent(IntPtr.Zero);
 			Alc.DestroyContext(context);
 			Alc.CloseDevice(audioDevice);
 
-			void printIntensity(double intensity)
-			{
-				Console.CursorVisible = false;
-				ClearCurrentConsoleLine();
-				int characterCount = (int)Math.Round(Console.WindowWidth * intensity);
-				for (int i = 0; i < characterCount; i++)
-				{
-					double value = (double)i / Console.WindowWidth;
-					if (value > 0.66)
-						Console.ForegroundColor = ConsoleColor.Red;
-					else if (value > 0.33)
-						Console.ForegroundColor = ConsoleColor.Yellow;
-					else
-						Console.ForegroundColor = ConsoleColor.Green;
-					Console.Write('■');
-				}
-			}
+		}
 
-			void resetConsoleSettings()
+		private static void printIntensity(double intensity)
+		{
+			Console.CursorVisible = false;
+			clearCurrentConsoleLine();
+			int characterCount = (int)Math.Round(Console.WindowWidth * intensity);
+			for (int i = 0; i < characterCount; i++)
 			{
-				Console.ResetColor();
-				Console.CursorVisible = true;
-			}
-
-			double calculateRMS(short[] values, int count)
-			{
-				if (count <= 0 || count > values.Length) throw new ArgumentOutOfRangeException(nameof(count), "Argument must be positive integer lower that the values array length.");
-				double sqrSum = 0;
-				for (int i = 0; i < count; i++)
-				{
-					sqrSum += (double)values[i] * values[i];
-				}
-				double sqrSumAvg = sqrSum / count;
-				return Math.Sqrt(sqrSumAvg);
+				double value = (double)i / Console.WindowWidth;
+				if (value > 0.66)
+					Console.ForegroundColor = ConsoleColor.Red;
+				else if (value > 0.33)
+					Console.ForegroundColor = ConsoleColor.Yellow;
+				else
+					Console.ForegroundColor = ConsoleColor.Green;
+				Console.Write('■');
 			}
 		}
 
-		public static void ClearCurrentConsoleLine()
+		private static void resetConsoleSettings()
+		{
+			Console.ResetColor();
+			Console.CursorVisible = true;
+		}
+
+		private static double calculateRMS(short[] values, int count)
+		{
+			if (count <= 0 || count > values.Length) throw new ArgumentOutOfRangeException(nameof(count), "Argument must be positive integer lower that the values array length.");
+			double sqrSum = 0;
+			for (int i = 0; i < count; i++)
+			{
+				sqrSum += (double)values[i] * values[i];
+			}
+			double sqrSumAvg = sqrSum / count;
+			return Math.Sqrt(sqrSumAvg);
+		}
+
+		private static void clearCurrentConsoleLine()
 		{
 			int currentLineCursor = Console.CursorTop;
 			Console.SetCursorPosition(0, Console.CursorTop);
---
+++ ClapDetector/WavFile.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ClapDetector
+{
+	class WavFile
+	{
+		public int SamplesPerSecond { get; set; } // frequency
+		public int SamplesTotalCount { get; set; }
+		public string Filename { get; private set; }
+		public byte[] MetaData { get; set; }
+
+
+		public WavFile(string filename)
+		{
+			SamplesTotalCount = SamplesPerSecond = -1;
+			Filename = filename;
+			MetaData = null;
+		}
+
+		public bool ReadMono16bit(out short[] samples)
+		{
+			samples = null;
+
+			using (FileStream fileStream = File.Open(Filename, FileMode.Open))
+			using (BinaryReader reader = new BinaryReader(fileStream))
+			{
+				// header (8 + 4 bytes):
+
+				byte[] riffId = reader.ReadBytes(4);    // "RIFF"
+				int fileSize = reader.ReadInt32();      // size of entire file
+				byte[] typeId = reader.ReadBytes(4);    // "WAVE"
+
+				if (Encoding.ASCII.GetString(typeId) != "WAVE") return false;
+
+				// chunk 1 (8 + 16 or 18 bytes):
+
+				byte[] fmtId = reader.ReadBytes(4);     // "fmt "
+				int fmtSize = reader.ReadInt32();       // size of chunk in bytes
+				int fmtCode = reader.ReadInt16();       // 1 - for PCM
+				int channels = reader.ReadInt16();      // 1 - mono, 2 - stereo
+				int sampleRate = reader.ReadInt32();    // sample rate per second
+				int byteRate = reader.ReadInt32();      // bytes per second
+				int dataAlign = reader.ReadInt16();     // data align
+				int bitDepth = reader.ReadInt16();      // 8, 16, 24, 32, 64 bits
+
+				if (fmtCode != 1) return false;     // not PCM
+				if (channels != 1) return false;    // only Mono files in this version
+				if (bitDepth != 16) return false;   // only 16-bit in this version
+
+				if (fmtSize == 18) // fmt chunk can be 16 or 18 bytes
+				{
+					int fmtExtraSize = reader.ReadInt16();  // read extra bytes size
+					reader.ReadBytes(fmtExtraSize);         // skip over "INFO" chunk
+				}
+
+				// chunk 2 (8 bytes):
+
+				byte[] dataId = reader.ReadBytes(4);    // "data"
+				int dataSize = reader.ReadInt32();      // size of audio data
+
+				Debug.Assert(Encoding.ASCII.GetString(dataId) == "data", "Data chunk not found!");
+
+				SamplesPerSecond = sampleRate;                  // sample rate (usually 44100)
+				SamplesTotalCount = dataSize;  // total samples count in audio data
+
+				// audio data:
+
+				samples = new short[SamplesTotalCount];
+
+				for (int i = 0; i < SamplesTotalCount; i += 1)
+				{
+					samples[i] = reader.ReadInt16();
+				}
+
+				// metadata:
+
+				long moreBytes = reader.BaseStream.Length - reader.BaseStream.Position;
+
+				if (moreBytes > 0)
+				{
+					MetaData = reader.ReadBytes((int)moreBytes);
+				}
+			}
+			return true;
+		}
+
+		public bool ReadStereo16bit(ref short[] L, ref short[] R)
+		{
+			using (FileStream fileStream = File.Open(Filename, FileMode.Open))
+			using (BinaryReader reader = new BinaryReader(fileStream))
+			{
+				// header (8 + 4 bytes):
+
+				byte[] riffId = reader.ReadBytes(4);    // "RIFF"
+				int fileSize = reader.ReadInt32();      // size of entire file
+				byte[] typeId = reader.ReadBytes(4);    // "WAVE"
+
+				if (Encoding.ASCII.GetString(typeId) != "WAVE") return false;
+
+				// chunk 1 (8 + 16 or 18 bytes):
+
+				byte[] fmtId = reader.ReadBytes(4);     // "fmt "
+				int fmtSize = reader.ReadInt32();       // size of chunk in bytes
+				int fmtCode = reader.ReadInt16();       // 1 - for PCM
+				int channels = reader.ReadInt16();      // 1 - mono, 2 - stereo
+				int sampleRate = reader.ReadInt32();    // sample rate per second
+				int byteRate = reader.ReadInt32();      // bytes per second
+				int dataAlign = reader.ReadInt16();     // data align
+				int bitDepth = reader.ReadInt16();      // 8, 16, 24, 32, 64 bits
+
+				if (fmtCode != 1) return false;     // not PCM
+				if (channels != 2) return false;    // only Stereo files in this version
+				if (bitDepth != 16) return false;   // only 16-bit in this version
+
+				if (fmtSize == 18) // fmt chunk can be 16 or 18 bytes
+				{
+					int fmtExtraSize = reader.ReadInt16();  // read extra bytes size
+					reader.ReadBytes(fmtExtraSize);         // skip over "INFO" chunk
+				}
+
+				// chunk 2 (8 bytes):
+
+				byte[] dataId = reader.ReadBytes(4);    // "data"
+				int dataSize = reader.ReadInt32();      // size of audio data
+
+				Debug.Assert(Encoding.ASCII.GetString(dataId) == "data", "Data chunk not found!");
+
+				SamplesPerSecond = sampleRate;                  // sample rate (usually 44100)
+				SamplesTotalCount = dataSize / (bitDepth / 8);  // total samples count in audio data
+
+				// audio data:
+
+				L = R = new short[SamplesTotalCount / 2];
+
+				for (int i = 0, s = 0; i < SamplesTotalCount; i += 2)
+				{
+					L[s] = reader.ReadInt16();
+					R[s] = reader.ReadInt16();
+					s++;
+				}
+
+				// metadata:
+
+				long moreBytes = reader.BaseStream.Length - reader.BaseStream.Position;
+
+				if (moreBytes > 0)
+				{
+					MetaData = reader.ReadBytes((int)moreBytes);
+				}
+			}
+			return true;
+		}
+
+		public void WriteMono16bit(ReadOnlySpan<short> samples)
+		{
+			Debug.Assert((SamplesTotalCount != -1) && (SamplesPerSecond != -1), "No sample count or sample rate info!");
+
+			using (FileStream fileStream = File.Create(Filename))
+			using (BinaryWriter writer = new BinaryWriter(fileStream))
+			{
+				int fileSize = 44 + SamplesTotalCount * 2;
+
+				if (MetaData != null)
+				{
+					fileSize += MetaData.Length;
+				}
+
+				// header:
+
+				writer.Write(Encoding.ASCII.GetBytes("RIFF"));  // "RIFF"
+				writer.Write((int)fileSize);                  // size of entire file with 16-bit data
+				writer.Write(Encoding.ASCII.GetBytes("WAVE"));  // "WAVE"
+
+				// chunk 1:
+
+				writer.Write(Encoding.ASCII.GetBytes("fmt "));  // "fmt "
+				writer.Write((int)16);                        // size of chunk in bytes
+				writer.Write((short)1);                         // 1 - for PCM
+				writer.Write((short)1);                         // only Mono files in this version
+				writer.Write((int)SamplesPerSecond);          // sample rate per second (usually 44100)
+				writer.Write((int)(2 * SamplesPerSecond));    // bytes per second (usually 88200)
+				writer.Write((short)2);                         // data align 4 bytes (2 bytes sample stereo)
+				writer.Write((short)16);                        // only 16-bit in this version
+
+				// chunk 2:
+
+				writer.Write(Encoding.ASCII.GetBytes("data"));  // "data"
+				writer.Write((int)SamplesTotalCount);   // size of audio data 16-bit
+
+				// audio data:
+
+				for (int i = 0; i < SamplesTotalCount; i += 1)
+				{
+					writer.Write(samples[i]);
+				}
+
+				// metadata:
+
+				if (MetaData != null)
+				{
+					writer.Write(MetaData);
+				}
+			}
+		}
+
+		public void WriteStereo16bit(short[] L, short[] R)
+		{
+			Debug.Assert((SamplesTotalCount != -1) && (SamplesPerSecond != -1), "No sample count or sample rate info!");
+
+			using (FileStream fileStream = File.Create(Filename))
+			using (BinaryWriter writer = new BinaryWriter(fileStream))
+			{
+				int fileSize = 44 + SamplesTotalCount * 2;
+
+				if (MetaData != null)
+				{
+					fileSize += MetaData.Length;
+				}
+
+				// header:
+
+				writer.Write(Encoding.ASCII.GetBytes("RIFF"));  // "RIFF"
+				writer.Write((int)fileSize);                  // size of entire file with 16-bit data
+				writer.Write(Encoding.ASCII.GetBytes("WAVE"));  // "WAVE"
+
+				// chunk 1:
+
+				writer.Write(Encoding.ASCII.GetBytes("fmt "));  // "fmt "
+				writer.Write((int)16);                        // size of chunk in bytes
+				writer.Write((short)1);                         // 1 - for PCM
+				writer.Write((short)2);                         // only Stereo files in this version
+				writer.Write((int)SamplesPerSecond);          // sample rate per second (usually 44100)
+				writer.Write((int)(4 * SamplesPerSecond));    // bytes per second (usually 176400)
+				writer.Write((short)4);                         // data align 4 bytes (2 bytes sample stereo)
+				writer.Write((short)16);                        // only 16-bit in this version
+
+				// chunk 2:
+
+				writer.Write(Encoding.ASCII.GetBytes("data"));  // "data"
+				writer.Write((int)(SamplesTotalCount * 2));   // size of audio data 16-bit
+
+				// audio data:
+
+				for (int i = 0, s = 0; i < SamplesTotalCount; i += 2)
+				{
+					writer.Write(L[s]);
+					writer.Write(R[s]);
+					s++;
+				}
+
+				// metadata:
+
+				if (MetaData != null)
+				{
+					writer.Write(MetaData);
+				}
+			}
+		}
+	}
+}
