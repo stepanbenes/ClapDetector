90fce06c1ff8ce194ab671e31d9a2469ca4b80f5
Štěpán Beneš <benes.stepan@gmail.com>
2019-07-25 13:21:56 +0200

    Add record mode

--- ClapDetector/Program.cs
+++ ClapDetector/Program.cs
@@ -1,7 +1,5 @@
-﻿using OpenAL;
-using SoundFingerprinting.Audio;
-using SoundFingerprinting.Builder;
-using SoundFingerprinting.Configuration;
+﻿using NAudio.Dsp;
+using OpenAL;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -13,6 +11,7 @@ namespace ClapDetector
 	{
 		static async Task Main(string[] args)
 		{
+
 			// =============================================================================================================
 			// inspired by: https://stackoverflow.com/questions/4087727/openal-how-to-create-simple-microphone-echo-programm
 
@@ -25,6 +24,7 @@ namespace ClapDetector
 			Alc.MakeContextCurrent(context);
 
 			const int frequency = 44100;
+			const int keywordLength = (int)(frequency * 1.1);
 
 			var captureDevice = Alc.CaptureOpenDevice(devicename: null, frequency, format: Al.FormatMono16, buffersize: frequency);
 
@@ -32,80 +32,44 @@ namespace ClapDetector
 
 			Console.WriteLine($"{deviceName} is open for capture.");
 
-			Alc.CaptureStart(captureDevice);
-
-			Console.WriteLine("listening for keywords..."); // need new line for intensity visualization
-
-			short[] buffer = new short[frequency];
-			const int keywordLength = (int)(frequency * 2);
-			double? previousRMS = null;
-
-			List<short> keywordSamples = new List<short>(capacity: keywordLength);
-			bool isRecording = false;
-			unsafe
+			if (args.Length > 0 && args[0] == "record")
 			{
-				fixed (int* samplesAvailable = new int[1])
-				fixed (short* bufferPointer = buffer)
+				while (true)
 				{
-					IntPtr samplesAvailablePointer = new IntPtr((void*)samplesAvailable);
-					int captureCount = 2048; // (captureCount / frequency) seconds
-					while (keywordSamples.Count < keywordLength)
+					Console.Write("Enter keyword name: ");
+					string keywordName = Console.ReadLine();
+					if (string.IsNullOrWhiteSpace(keywordName)) // exit if nothing entered
+						break;
+
+					Alc.CaptureStart(captureDevice);
+					Console.WriteLine("recording keyword..."); // need new line for intensity visualization
+
+					short[] samples = captureSamples(captureDevice, frequency, keywordLength);
+
+					Alc.CaptureStop(captureDevice);
+
+					// write output
 					{
-						Alc.GetIntegerv(captureDevice, Alc.EnumCaptureSamples, 1, samplesAvailablePointer);
-						int samplesAvailableCount = samplesAvailable[0];
-						if (samplesAvailableCount > captureCount)
-						{
-							Alc.CaptureSamples(captureDevice, (void*)bufferPointer, samplesAvailableCount);
-
-							// TODO: ... do something with the buffer
-							double rms = calculateRMS(buffer, samplesAvailableCount);
-
-							double maxAmplitude = short.MaxValue;
-							double intensity = rms / maxAmplitude;
-							printIntensity(intensity);
-
-							// compare current rms with previous values to find beginning of a keyword
-							const double factor = 10;
-							if (rms > previousRMS * factor) // beginning of window
-							{
-								isRecording = true;
-								//Console.Beep(10000, 10);
-							}
-
-							if (isRecording)
-							{
-								for (int i = 0; i < samplesAvailableCount; i++)
-								{
-									keywordSamples.Add(buffer[i]);
-								}
-							}
-
-							previousRMS = rms;
-						}
+						string keywordFilename = $"{keywordName}.wav";
+						Debug.Assert(samples.Length >= keywordLength);
+						var wavFile = new WavFile(keywordFilename) { SamplesPerSecond = frequency, SamplesTotalCount = keywordLength /* trim samples */ };
+						wavFile.WriteMono16bit(samples);
+						Console.WriteLine($"'{keywordFilename}' was saved.");
 					}
 				}
 			}
-			Alc.CaptureStop(captureDevice);
-			resetConsoleSettings();
-
-			string keywordFilename = "keyword.wav";
-			
-			// write output
+			else
 			{
-				Debug.Assert(keywordSamples.Count >= keywordLength);
-				var wavFile = new WavFile(keywordFilename) { SamplesPerSecond = frequency, SamplesTotalCount = keywordLength /* trim samples */ };
-				wavFile.WriteMono16bit(keywordSamples.ToArray());
+				Alc.CaptureStart(captureDevice);
+				Console.WriteLine("listening for keyword..."); // need new line for intensity visualization
+				while (true)
+				{
+					short[] samples = captureSamples(captureDevice, frequency, keywordLength);
+
+					var fftData = calculateFFT(samples);
+				}
+				Alc.CaptureStop(captureDevice);
 			}
-
-			// read output
-			{
-				IAudioService audioService = new SoundFingerprintingAudioService(); // default audio library
-				var fingerprints = await FingerprintCommandBuilder.Instance.BuildFingerprintCommand().From(keywordFilename).UsingServices(audioService).Hash();
-
-			}
-
-			// TODO: real-time querying
-
 
 			// clean up
 			Alc.CaptureCloseDevice(captureDevice);
@@ -113,6 +77,59 @@ namespace ClapDetector
 			Alc.DestroyContext(context);
 			Alc.CloseDevice(audioDevice);
 
+		}
+
+		private unsafe static short[] captureSamples(IntPtr captureDevice, int frequency, int sampleCount)
+		{
+			List<short> samples = new List<short>(capacity: sampleCount);
+
+			bool isRecording = false;
+			double? previousRMS = null;
+			short[] buffer = new short[frequency];
+			fixed (int* samplesAvailable = new int[1])
+			fixed (short* bufferPointer = buffer)
+			{
+				while (samples.Count < sampleCount)
+				{
+					IntPtr samplesAvailablePointer = new IntPtr((void*)samplesAvailable);
+					Alc.GetIntegerv(captureDevice, Alc.EnumCaptureSamples, 1, samplesAvailablePointer);
+					int samplesAvailableCount = samplesAvailable[0];
+					const int captureCount = 2048;
+					if (samplesAvailableCount > captureCount)
+					{
+						Alc.CaptureSamples(captureDevice, (void*)bufferPointer, samplesAvailableCount);
+
+						// TODO: ... do something with the buffer
+						double rms = calculateRMS(buffer, samplesAvailableCount);
+
+						double maxAmplitude = short.MaxValue;
+						double intensity = rms / maxAmplitude;
+						printIntensity(intensity);
+
+						// compare current rms with previous values to find beginning of a keyword
+						const double factor = 10;
+						if (rms > previousRMS * factor) // beginning of window
+						{
+							isRecording = true;
+							//Console.Beep(10000, 10);
+						}
+
+						if (isRecording)
+						{
+							for (int i = 0; i < samplesAvailableCount; i++)
+							{
+								samples.Add(buffer[i]);
+							}
+						}
+
+						previousRMS = rms;
+					}
+				}
+			}
+
+			resetConsoleSettings();
+
+			return samples.ToArray();
 		}
 
 		private static void printIntensity(double intensity)
@@ -157,6 +174,30 @@ namespace ClapDetector
 			Console.SetCursorPosition(0, Console.CursorTop);
 			Console.Write(new string(' ', Console.WindowWidth));
 			Console.SetCursorPosition(0, currentLineCursor);
+		}
+
+		private static double[] calculateFFT(short[] dataPcm)
+		{
+			// the PCM size to be analyzed with FFT must be a power of 2
+			int fftPoints = 2;
+			while (fftPoints * 2 <= dataPcm.Length)
+				fftPoints *= 2;
+
+			// apply a Hamming window function as we load the FFT array then calculate the FFT
+			Complex[] fftFull = new Complex[fftPoints];
+			for (int i = 0; i < fftPoints; i++)
+				fftFull[i].X = (float)(dataPcm[i] * FastFourierTransform.HammingWindow(i, fftPoints));
+			FastFourierTransform.FFT(true, (int)Math.Log(fftPoints, 2.0), fftFull);
+
+			// copy the complex values into the double array that will be plotted
+			double[] dataFft = new double[fftPoints / 2];
+			for (int i = 0; i < fftPoints / 2; i++)
+			{
+				double fftLeft = Math.Abs(fftFull[i].X + fftFull[i].Y);
+				double fftRight = Math.Abs(fftFull[fftPoints - i - 1].X + fftFull[fftPoints - i - 1].Y);
+				dataFft[i] = fftLeft + fftRight;
+			}
+			return dataFft;
 		}
 	}
 }
